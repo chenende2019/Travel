9b7aec20849f57e1250466e07e58af7bb4bd6819
chenende <m17600201360@163.com>
2019-02-25 02:58:10 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import 'styles/reset.css';
 import 'styles/border.css';
 import 'styles/iconfont.css';
 import fastClick from 'fastclick';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 import Vue from 'vue';
+import 'swiper/dist/css/swiper.css';
 import App from './App';
 import router from './router';
 
 fastClick.attach(document.body);
 Vue.config.productionTip = false;
+
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.url"/>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+      },
+      swiperList: [{
+        id: '001',
+        url: 'http://img1.qunarzz.com/piao/fusion/1804/13/e1188b0d0f21b902.jpg_750x200_f85c782a.jpg',
+      },{
+        id: '002',
+        url: 'http://img1.qunarzz.com/piao/fusion/1712/51/36632b2cb850e902.jpg_750x200_fa4120db.jpg',
+      },{
+        id: '003',
+        url: 'http://img1.qunarzz.com/piao/fusion/1806/8b/2680212fadf30002.jpg_750x200_f85409ac.jpg',
+      },{
+        id: '004',
+        url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/61c3b6dc2a3b1593e82667a5d92a6706.jpg_750x200_1ec098e6.jpg',
+      }],
+    };
+  },
+};
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #ffffff
+  .wrapper
+    overflow hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.7%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
