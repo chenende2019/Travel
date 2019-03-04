2331705925986f74f963aaf9362fa5802c0e0861
chenende <m17600201360@163.com>
2019-03-04 23:10:54 +0800

    add search to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search">
+    <input class="search-img" type="text" placeholder="请输入要搜索的内容"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CiytSearch',
+};
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+    .search
+      line-height 0.1rem
+      background $bgColor
+      padding 0 .1rem .1rem
+      .search-img
+        box-sizing border-box
+        line-height .62rem
+        width 100%
+        border-radius .05rem
+        text-align center
+        color #666
+        padding 0 .1rem
+</style>
